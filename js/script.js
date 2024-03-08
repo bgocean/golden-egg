204a16317681b2b7f75796d1ba74745eb8642dcb
bgocean <bigocean700@gmail.com>
2024-03-09 01:58:00 +0200

    add tabs

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".swiper-container", {
+const swiper = new Swiper(".spec__slider", {
   // effect: 'coverflow',
   // grabCursor: true,
   // centeredSlides: true,
@@ -19,11 +19,40 @@ const swiper = new Swiper(".swiper-container", {
   //     prevEl: '.swiper-button-prev',
   // },
   loop: true,
+  speed: 800,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+});
+
+const swiperMenu = new Swiper(".menu__slider", {
+  slidesPerView: 2,
+  slidesPerColumn: 2,
   speed: 800,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+let tabs = document.querySelector(".tabs");
+let menuSlider = document.querySelectorAll(".menu__slider-block");
+
+tabs.addEventListener("click", function (e) {
+  if (e.target.classList.contains("tabs__item")) {
+    this.querySelectorAll(".tabs__item").forEach((item) =>
+      item.classList.remove("active")
+    );
+    e.target.classList.add("active");
+  }
+
+  menuSlider.forEach((item) => {
+    item.classList.remove("show");
+    let tabIndex = e.target.dataset.tab;
+    let thisSwiper = document.querySelector(tabIndex);
+    thisSwiper.classList.add("show");
+  });
 });
 
 window.addEventListener("load", overflowTabs);
